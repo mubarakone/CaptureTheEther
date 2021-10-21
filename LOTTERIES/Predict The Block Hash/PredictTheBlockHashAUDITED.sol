bee9d78ef3a06668c76e522c4be06105b7514908
mubarakone <52806204+mubarakone@users.noreply.github.com>
2021-10-20 21:30:55 -0700

    Create PredictTheBlockHashAUDITED.sol

---
+++ LOTTERIES/Predict The Block Hash/PredictTheBlockHashAUDITED.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.4.21;
+
+//       *****AUDITED*****
+
+contract RandomNumberConsumer is VRFConsumerBase {
+    
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    uint256 public randomResult;
+    
+    constructor() 
+        VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
+        ) public
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+    }
+    
+    function getRandomNumber(uint256 userProvidedSeed) public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee, userProvidedSeed);
+    }
+
+    /*
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
+    }
+}
+
+contract PredictTheBlockHashChallenge {
+    address guesser;
+    bytes32 guess;
+    uint256 settlementBlockNumber;
+    RandomNumberConsumer randomNumber = RandomNumberConsumer(msg.sender);
+
+    constructor() public payable {
+        require(msg.value == 1 ether);
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function lockInGuess(bytes32 hash) public payable {
+        require(guesser == 0);
+        require(msg.value == 1 ether);
+
+        guesser = msg.sender;
+        guess = hash;
+        settlementBlockNumber = block.number + 1;
+    }
+
+    function settle() public {
+        require(msg.sender == guesser);
+        require(block.number > settlementBlockNumber);
+
+        bytes32 answer = bytes32(keccak256(abi.encodePacked(randomNumber.getRandomNumber(settlementBlockNumber))));
+
+        guesser = 0;
+        if (guess == answer) {
+            msg.sender.transfer(2 ether);
+        }
+    }
+}

